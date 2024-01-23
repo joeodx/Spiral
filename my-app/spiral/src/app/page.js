f955e2b319aa589ec549ea9be467c6a3e274de80
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-01-23 16:43:57 +0000

    started

---
+++ my-app/spiral/src/app/page.js
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 class="text-center text-2xl">Spiral</h1>
+      <h2 class="text-center text-lg">Stop your negative thoughts in their tracks</h2>
+    </main>
+  );
+}
