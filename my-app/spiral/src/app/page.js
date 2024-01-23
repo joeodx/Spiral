80357416189cb36b3369f7163a8944cbb11bdb52
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-01-23 17:34:40 +0000

    add

--- my-app/spiral/src/app/page.js
+++ my-app/spiral/src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 class="text-center text-2xl">Spiral</h1>
+      <h1 class="transition ease-in-out text-center text-2xl">Spiral</h1>
       <h2 class="text-center text-lg">Stop your negative thoughts in their tracks</h2>
     </main>
   );
