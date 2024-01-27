e4878f579408e24526d396b66ecf131aa5380196
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-01-27 19:49:05 +0000

    updated

--- my-app/spiral/src/app/layout.js
+++ my-app/spiral/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="my-app\spiral\public\spiral.png" />
+      <link rel="icon" href="/my-app/spiral/public/spiral.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
